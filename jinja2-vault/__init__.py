be74269ee82b7511142477c319794375310c9804
Jaydeep Dave <jaydipdave@gmail.com>
2019-10-30 12:28:32 -0400

    Added option to write secret directly to a file

--- jinja2-vault/__init__.py
+++ jinja2-vault/__init__.py
@@ -4,7 +4,7 @@ import logging
 from jinja2 import lexer, nodes
 from jinja2.ext import Extension
 
-debug_level = os.getenv("JINJA2_VAULT_DEBUG", "WARNING")
+debug_level = os.getenv("JINJA2_VAULT_DEBUG", "ERROR")
 logging.basicConfig(level=debug_level)
 logger = logging.getLogger(__name__)
 
@@ -28,19 +28,32 @@ class VaultExtension(Extension):
             mount = parser.parse_expression()
         else:
             mount = "secret"
+        parser.stream.skip_if('comma')
+        if parser.stream.skip_if('name:to_file'):
+            parser.stream.skip(1)
+            to_file = parser.parse_expression()
+        else:
+            to_file = None
 
         args = [path,key,mount]
+        if to_file:
+            args.append(to_file)
         return nodes.Output([
             nodes.MarkSafeIfAutoescape(self.call_method('get_secret', args))
         ]).set_lineno(lineno)
 
 
-    def get_secret(self, path, key, mount):
+    def get_secret(self, path, key, mount, to_file=None):
         # Try to connect
         try:
             hostname = os.getenv("VAULT_ADDR", "http://localhost:8200")
             token = os.getenv("VAULT_TOKEN", "myroot")
-            client = hvac.Client(hostname,token)
+            verify = False if os.getenv("VAULT_SKIP_VERIFY", "0") == "1" else True
+            client = hvac.Client(
+                                    url=hostname,
+                                    token=token,
+                                    verify=verify
+                                )
             # Verfiy authentication
             if not client.is_authenticated():
                 logger.warning(
@@ -53,8 +66,6 @@ class VaultExtension(Extension):
             client.adapter.close()
             return "failed-to-connect"
         
-
-
         # Load data
         try:
             secret = client.secrets.kv.v2.read_secret_version(path=path, mount_point=mount)
@@ -68,6 +79,11 @@ class VaultExtension(Extension):
         # return data
         if key in secret["data"]["data"]:
             client.adapter.close()
+            if to_file:
+                file = open(to_file,"w+")
+                file.write(str(secret["data"]["data"][key])) 
+                file.close()
+                return to_file
             return secret["data"]["data"][key]
         else:
             logger.warning("No key {0} found".format(path))
